721bffe4cbf8a820d2bc895aa969db4b76c468cb
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-05-09 13:50:06 +0500

    layout fixes

--- src/components/Shared/Layout/index.jsx
+++ src/components/Shared/Layout/index.jsx
@@ -3,7 +3,7 @@ import Footer from "../Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-background ">
+    <div className="bg-background max-w-360 mx-auto">
       <Navbar />
       <div className="">{children}</div>
       <Footer />
